8442b6d6e9d13e8d69f5eb62a19734719a7367e4
cyrusn <cyrusncy@gmail.com>
2018-11-21 02:17:01 +0800

    add help text with argparse

--- convertReferenceLinkToInlineLink.py
+++ convertReferenceLinkToInlineLink.py
@@ -1,8 +1,6 @@
 #!/usr/local/bin/python3
-import sys
 import re
-
-query = sys.argv[1]
+from math import log, floor
 
 
 def convertReferenceLinkToInlineLink(content):
@@ -22,6 +20,71 @@ def convertReferenceLinkToInlineLink(content):
     content = content.replace("\n<!-- reference links -->\n\n", "")
     return content
 
-result = convertReferenceLinkToInlineLink(query)
 
-print(result, end='')
+def convertInlineLinkToReferenceLink(content):
+
+    MARKDOWN_LINK_PATTERN = r"\[\!\[([^\[]+?)\]\(([^\#].+?)\)\]\(([^\#].+?)\)|\[([^\[]+?)\]\(([^\#].+?)\)"
+
+    archors = []
+
+    index = 0
+
+    mdlinks = re.finditer(MARKDOWN_LINK_PATTERN, content)
+    lst = list(mdlinks)
+    size = len(lst)
+
+    for i, link in enumerate(lst):
+        index += 1
+        pad = int(floor(log(max(size, 1), 10))) + 1
+
+        if link.group(1):
+            formattedArchorImageString = "[@{:0{pad}d}-img]: {}".format(
+                index, link.group(2), pad=pad)
+            formattedArchorLinkString = "[@{:0{pad}d}-url]: {}".format(
+                index, link.group(3), pad=pad)
+
+            archors.append(formattedArchorImageString)
+            archors.append(formattedArchorLinkString)
+
+            formattedString = "[![{}][@{:0{pad}d}-img]][@{:0{pad}d}-url]".format(
+                link.group(1), index, index, pad=pad)
+
+        else:
+            formattedArchorString = "[@{:0{pad}d}]: {}".format(
+                index, link.group(5), pad=pad)
+            archors.append(formattedArchorString)
+
+            formattedString = "[{}][@{:0{pad}d}]".format(
+                link.group(4), index, pad=pad)
+
+        content = content.replace(link.group(0), formattedString)
+
+    content += "\n<!-- reference links -->\n\n"
+    for a in archors:
+        content += a + "\n"
+
+    return content
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description='Markdown Link converter')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        '-r', '--reference',
+        help='convert reference link to inline link',
+        action="store_true"
+    )
+    group.add_argument(
+        '-i', '--inline',
+        help='convert inline link to reference link',
+        action='store_true'
+    )
+    parser.add_argument('content', help="Markdown content")
+    args = parser.parse_args()
+
+    if args.reference:
+        result = convertReferenceLinkToInlineLink(args.content)
+    elif args.inline:
+        result = convertInlineLinkToReferenceLink(args.content)
+    print(result, end='')
